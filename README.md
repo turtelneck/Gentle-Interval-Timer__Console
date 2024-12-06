dcd2f807bc010606c0cdf153b5725576b4b63c9c
turtelneck <65428036+turtelneck@users.noreply.github.com>
2024-12-05 19:53:43 -0600

    Updated ReadMe
    Added program description
    Removed an obsolete thread lock

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Gentle Interval Timer
+# Gentle Hands-Free Interval Timer
  
+Set a timer, start some music, begin a task of your choice.
+
+When the timer finishes, it will quietly crossfade into nature sounds of your choice. Correct your posture, close your eyes, take a couple of deep breaths; some seconds later your music will fade back in and you can return to your task.
+
+Operates identically without music playing. An unobtrusive audio cue to re-center yourself. Repeats as many times as you like, or until you power it down.
--- interval_timer.py
+++ interval_timer.py
@@ -16,26 +16,23 @@ interval_seconds = 25
 interval_count = 2
 sound_file = "wave_sounds.wav"
 
-# Ensures timer never outpaces fade_pause_play()
-fade_lock = threading.Lock()
 
 def fade_pause_play(sound_file_path):
-    with fade_lock:
-        # print("Interval over, fading out music")
-        result = subprocess.run(["osascript", "-e", fadeout_script], text=True, capture_output=True)
-        prev_vol = result.stdout.strip()
+    # print("Interval over, fading out music")
+    result = subprocess.run(["osascript", "-e", fadeout_script], text=True, capture_output=True)
+    prev_vol = result.stdout.strip()
 
-        # print("Pausing music...")
-        subprocess.run(['osascript', '-e', pause_script, str(prev_vol)])
+    # print("Pausing music...")
+    subprocess.run(['osascript', '-e', pause_script, str(prev_vol)])
 
-        # print("Playing chosen sound file")
-        playsound(sound_file_path)
+    # print("Playing chosen sound file")
+    playsound(sound_file_path)
 
-        # print("Unpausing music...")
-        subprocess.run(['osascript', '-e', play_script])
+    # print("Unpausing music...")
+    subprocess.run(['osascript', '-e', play_script])
 
-        # print("Fading music in")
-        subprocess.run(['osascript', '-e', fadein_script, str(prev_vol)])
+    # print("Fading music in")
+    subprocess.run(['osascript', '-e', fadein_script, str(prev_vol)])
 
 
 def timer(interval, sound_file_path):
@@ -46,9 +43,9 @@ def timer(interval, sound_file_path):
         
         fade_thread = threading.Thread(target=fade_pause_play, args=(sound_file_path,))
         fade_thread.start()
-        fade_thread.join() # ensures timer never outpaces fade_pause_play()
+        fade_thread.join() # ensures timer never outpaces fade_pause_play
         
-        # timer waits for time remaining, accounting for fade_pause_play() execution time
+        # timer waits for time remaining, accounting for fade_pause_play execution time
         elapsed_time = time.time() - start_time
         print(elapsed_time)
         time_to_wait = max(0, interval - elapsed_time)
@@ -71,5 +68,5 @@ if __name__ == "__main__":
 
 
 # BUGS:
-# if volume starts muted, fadeout AppleScript divides by zero
+# if volume starts muted, fadeout AppleScript divides by zero (not fatal)
 # need to check if music was playing in the first place, lest ye unpause what ought remain paused
